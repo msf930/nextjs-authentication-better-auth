6b4d54515ecf6c2a46707a901f58618fbd5173bf
Matt Folsom <msf930@gmail.com>
2026-01-01 14:03:03 -0700

    re generate prisma orm

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,11 @@
 
-import { PrismaClient } from "./generated/prisma";
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
 
+const connectionString = `${process.env.DATABASE_URL}`
 
-export const prisma = new PrismaClient({ datasourceUrl: process.env.DATABASE_URL })
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
